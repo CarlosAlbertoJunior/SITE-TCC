36ed660cfa8127b86258a23a5dc99c4f468dd76d
Carlos Alberto Jr <albertocloud.carlos@gmail.com>
2024-12-12 10:37:23 -0300

    Att home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./Home.css";
+import Imagem from "../imagem/HOME/01.png"
+
 
 const Home = () => {
-  return (
-    <section>
-      <h1>Bem-vindo ao Portal da Transparência</h1>
-      <p>Explore dados dos vereadores e da prefeitura. Participe dos debates no fórum!</p>
-    </section>
-  );
+    return (
+        <body>
+            <section className="inicio">
+                <div>
+                    <div className="texto-inicio">
+                        <h1>Bem-vindo ao Portal da Transparência</h1>
+                    </div>
+                    <div className="imagem-inicio">
+                        <img src={Imagem} alt="Apresentação" />
+                    </div>
+                </div>
+            </section>
+        </body>
+    );
 };
 
 export default Home;
